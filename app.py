6dfcd801f930c8271de0fa78458a978088cb2e27
Zehui17 <111267478+Zehui17@users.noreply.github.com>
2022-08-15 14:31:12 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,50 +1,41 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
-
-
-
 # In[1]:
 
 
-from flask import Flask, render_template, request
-app = Flask(__name__)
+from flask import Flask,render_template,request
 
 
 # In[2]:
 
 
-import  joblib
+app = Flask(__name__)
 
 
-@app.route("/", methods = ["GET", "POST"])
+# In[3]:
+
+
+import joblib
+@app.route("/",methods=["GET","POST"])#decorator
 def index():
-    if request.method == 'POST':
-        rates = float(request.form.get('rates'))
+    if request.method == "POST":
+        rates = float(request.form.get("rates"))
         print(rates)
-        model1 = joblib.load('regression_DBS')
+        model1 = joblib.load("regression_DBS")
         r1 = model1.predict([[rates]])
         model2 = joblib.load("tree_DBS")
         r2 = model2.predict([[rates]])
-        return (render_template('index.html', result1 = r1, result2 = r2))
-    else:
-        return (render_template('index.html', result1 = 'waiting', result2 = 'waiting'))
+        return(render_template("index.html",result1=r1,result2=r2))
+    else:#before press "enter"
+        return(render_template("index.html",result1="waiting",result2="waiting"))
 
 
 # In[ ]:
 
 
 if __name__ == "__main__":
-    app.run()
-
-
-# In[ ]:
-
-
-#app.run(host = '127.0.0.1', port = int("80"))
+    app.run()#host="127.0.0.1",port=int("80")
 
 
 # In[ ]:
